a3f6552d1016ef21f0b3353ced29cef686a669d8
kajalmaurya <kajalstudy86@gmail.com>
2024-02-23 22:33:27 +0530

    nothing

--- client/src/Components/Login_components/Forgot_password.jsx
+++ client/src/Components/Login_components/Forgot_password.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Forgot_password = () => {
   return (
-    <div>Forgot_password</div>
+    <styledForget>
+    <div className='container'>
+      <h1>Forget Password</h1>
+      <label htmlFor="email">Email</label>
+            <br />
+            <br />
+            <input name="email" id="email" className="contact-inputs" type="text" required />
+    </div>
+
+    </styledForget>
   )
 }
 
