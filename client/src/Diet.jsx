c94adefc63fa6207ea1a239a01dfe8a01699afeb
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2024-03-10 22:30:42 +0530

    workout page done

--- client/src/Diet.jsx
+++ client/src/Diet.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 
 const Diet = () => {
@@ -33,7 +33,8 @@ const Diet = () => {
 
       </div>
       <div id="blue" className='container'>
-
+        <FontAwesomeIcon style={{color:'#CC3D00',fontSize:'2rem', marginTop:'2vh',marginLeft:'90%'}} icon={faFile} />
+        <img style={{ width: "90%", marginTop: "-8vh", height: "80vh", marginLeft:'5%' }} src="./Resorces/Cal1500.jpg" alt="" />
 
       </div>
       <div id="green" className='container'>
@@ -44,42 +45,41 @@ const Diet = () => {
 
 
       </div>
-      
+
     </StyledDiet>
   );
 };
 
 const StyledDiet = styled.div`
-  background-color: #d04e17;
   #red {
     display: ${({ calory }) => (calory >= 600 && calory < 1500 ? 'flex' : 'none')};
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-    border: 1px solid rgba(0, 0, 0, 0.4); 
-    border-radius: 8px;
+    /* border: 1px solid rgba(0, 0, 0, 0.4); 
+    border-radius: 8px; */
   }
   #blue {
     display: ${({ calory }) => (calory >= 1500 && calory < 1800 ? 'flex' : 'none')};
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3); 
-    border: 1px solid rgba(0, 0, 0, 0.4); 
-    border-radius: 8px;
+    /* border: 1px solid rgba(0, 0, 0, 0.4); 
+    border-radius: 8px; */
   }
   #green {
     display: ${({ calory }) => (calory >= 1800 && calory < 2000 ? 'flex' : 'none')};
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3); /* Adjust shadow as needed */
-    border: 1px solid rgba(0, 0, 0, 0.4); /* Adjust border color as needed */
-    border-radius: 8px;
+    /* border: 1px solid rgba(0, 0, 0, 0.4);  */
+    /* border-radius: 8px; */
   }
   #yellow {
     display: ${({ calory }) => (calory >= 2000 && calory < 2500 ? 'flex' : 'none')};
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3); 
-    border: 1px solid rgba(0, 0, 0, 0.4);
-    border-radius: 8px;
+    /* border: 1px solid rgba(0, 0, 0, 0.4);
+    border-radius: 8px; */
   }
   .container {
+    background-color: white;
     margin: auto;
-    width: 80%;
+    width: 90%;
     min-height: 100vh;
-    background-color:#242426; 
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
