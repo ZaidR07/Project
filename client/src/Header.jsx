ee25ca6cf0090d50a715f8f6d0b7ada02dfb3e67
Zaid Saifur Rahman <102205754+ZaidR07@users.noreply.github.com>
2024-03-25 08:50:37 +0000

    Back to normal

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -45,22 +45,22 @@ const Header = ({ setIsLoggedIn }) => {
 
     if (barclicked) {
       mobilenavElement.style.cssText = `
-      height: 100vh;
-      background-color:#CC3D00 ;
-      background-image: none;
-      position:fixed ;
-      top: var(--navbarheight);
-      right: 0;
-      width: 50%;
-      flex-direction: column;
-      align-items: center;
-      padding-top: 15vh;
-      z-index: 99;
+        width: 50%;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        right: 0;
+        transform: translateX(100%);
+        display : none;
       `;
     }
     else {
       mobilenavElement.style.cssText = `
-        transform: translateX(100%);
+        width: 50%;
+        height: 100vh;
+        position: fixed;
+        right: 0;
+        transform: translateX(0);
         
         
         
@@ -314,7 +314,17 @@ const MainHeader = styled.header`
 // Responsiveness for mobile
 @media screen and (max-width: 480px) {
   .navbar{
-    
+    height: 100vh;
+    background-color:#CC3D00 ;
+    background-image: none;
+    position:fixed ;
+    top: var(--multinavheight);
+    right: 0;
+    width: 50%;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 15vh;
+    z-index: 99;
 
 
 
@@ -332,7 +342,7 @@ const MainHeader = styled.header`
 
   .topmobnav{
     width: 100%;
-    height: var(--navbarheight);
+    height: var(--multinavheight);
     background-color: #15191F;
     padding: 2%;
     position: fixed;
