19bca06aae6ea732a2a5e5dbf401f01050266157
Zaid Saifur Rahman <zaidrahman342@gmail.com>
2024-05-02 11:46:16 +0530

    For incorrect Password

--- client/src/Login_plus_register.jsx
+++ client/src/Login_plus_register.jsx
@@ -68,7 +68,7 @@ const Login_plus_register = ({ setIsLoggedIn , setAdminLoggedIn}) => {
                 localStorage.setItem('username', response.data.user.username);
                 navigate('/Home');
             }
-            if(response.data.userlogin === "false"){
+            if(!response.data.userlogin){
                 toast.error("Incorrect Username or password");
             }
             if(response.data.admin){
